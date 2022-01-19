ac604e0a31ea563beb8debef726611cfed2a7980
Thai <thaipro2k3@gmail.com>
2022-01-19 11:16:23 +0100

    b

--- src/main/kotlin/Contact.kt
+++ src/main/kotlin/Contact.kt
@@ -1,17 +1,12 @@
 import kotlin.time.measureTimedValue
 
-class Contact {
-
-    var name = "Unknown"
-    var lastName = "Unknown"
+class Contact (var _name: String = "Unknown", var _lastName: String = "Unknown", var _age: Int = -1) {
+    var name = _name
+    var lastName = _lastName
+    var age = _age
     var phoneNumber1 = 0
     var phoneNumber2 = 0
     var mail1 = ""
     var mail2 = ""
-/*
-    class Contact constructor(var _name: String)
-    {
-        name = _name
-    }
-*/
 }
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,15 @@
 import com.sun.jdi.IntegerValue
+import javax.swing.text.AbstractDocument
 
 fun main(args: Array<String>) {
 
-    var _contact = Contact()
 
     println("Thais telefonbok")
     var _function = -1
     //var _contactList: MutableList<Contact>
-    var _contactList = MutableList(0) {Contact() }
-    _contactList.add(Contact())
-    _contactList.add(Contact())
+    //var _contactList = MutableList(0) {Contact() }
 
-    _contactList[0].name = "Thai"
-   _contactList[1].name = "Thai2"
+    var _contactList = ArrayList<Contact>()
 
     fun PrintList()
     {
@@ -22,6 +19,8 @@ fun main(args: Array<String>) {
             println(_contact.name)
             print("Efternamn: ")
             println(_contact.lastName)
+            print("Age: ")
+            println(_contact.age)
             print("Telefonnummer: ")
             println(_contact.phoneNumber1)
             print("Telefonnummer: ")
@@ -32,25 +31,46 @@ fun main(args: Array<String>) {
             println(_contact.mail2)
         }
 
-
-
     }
 
-
-    while (_function != 0)
+    fun PrintMenu()
     {
+        println()
         println("1.Visa hela kontaktlistan")
         println("2.Lägga till en kontakt i kontakt listan")
         println("3.Ta bort en kontakt")
         println("4.Redigera en kontakt")
         println("0.Avsluta")
 
+    }
+
+    fun AddList()
+    {
+
+        println("Förnamn: ")
+        val _name :String =
+        println("Efternamn:")
+        val _aftername = readLine()
+        println("Age: ")
+        val _age = readLine()
+        //_contactList.add(Contact("Thai", "Bui", 10))
+        _contactList.add(Contact(_name))
+
+    }
+
+
+
+
+    while (_function != 0)
+    {
+        PrintMenu()
+
         _function = Integer.valueOf(readLine())
 
         when (_function)
         {
              1 -> PrintList()
-             2 -> println(_contactList[0].mail2)
+             2 -> AddList()
              3 -> println(_contactList[0].mail2)
              4 -> println(_contactList[0].mail2)
              5 -> println(_contactList[0].mail2)
